75ef9f5670d02a6096aba5061c3a3a6feb35e579
hb-rao34 <habibaashrafrao34@gmail.com>
2024-10-23 01:55:12 +0500

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
-"use client";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import { Spotlight } from "@/components/ui/Spotlight";
+import WhyChooseUs from '@/components/WhyChooseUs';
+import FeaturedWabinars from "@/components/FeaturedWabinars";
+import React from "react";
+import TestiMonialCard from "@/components/TestiMonialCard";
 
-import React from 'react';
-import HeroSection from '@/components/HeroSection';
-import FeaturedCourses from '@/components/FeaturedCourses';
-import TestiMonialCard from '@/components/TestiMonialCard';
-import FeaturedWebinars from '@/components/FeaturedWabinars';
-import WhyChooseUs from '@/components/WhyChooseUs'
-import Footer from '@/components/Footer';
 
 
-const HomePage = () => {
+
+export const page = () => {
   return (
-    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.07]'>
-      <HeroSection />
-      <FeaturedCourses />
-      <WhyChooseUs/>
-      <TestiMonialCard />
-      <FeaturedWebinars />
-      <Footer/>
+    <div>
       
-    </main>
-  );
+      <HeroSection/>
+      <FeaturedWabinars/>
+      <Spotlight/>
+      <WhyChooseUs/>
+      <FeaturedCourses/>
+      <TestiMonialCard/>
+    
+      <Footer/>
+
+
+    </div>
+  )
 }
 
-export default HomePage;
+export default page
