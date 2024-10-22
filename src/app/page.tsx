71fc55f00ea4338cffc63d030905236b03faf79b
hb-rao34 <habibaashrafrao34@gmail.com>
2024-10-23 02:25:21 +0500

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,20 @@
-import FeaturedCourses from "@/components/FeaturedCourses";
-import Footer from "@/components/Footer";
-import HeroSection from "@/components/HeroSection";
-import { Spotlight } from "@/components/ui/Spotlight";
+import HeroSection from '@/components/HeroSection';
+import { Spotlight } from '@/components/ui/Spotlight';
+import FeaturedCourses from '@/components/FeaturedCourses';
 import WhyChooseUs from '@/components/WhyChooseUs';
-import FeaturedWabinars from "@/components/FeaturedWabinars";
-import React from "react";
-import TestiMonialCard from "@/components/TestiMonialCard";
+import FeaturedWabinars from '@/components/FeaturedWabinars';
+import React from 'react'
 
+const page = () => {
+  return(
 
-
-
-export const page = () => {
-  return (
     <div>
-      
-      <HeroSection/>
-      <FeaturedWabinars/>
-      <Spotlight/>
-      <WhyChooseUs/>
-      <FeaturedCourses/>
-      <TestiMonialCard/>
-    
-      <Footer/>
 
+<HeroSection/>
+<Spotlight/>
+<FeaturedCourses/>
+<WhyChooseUs/>
+<FeaturedWabinars/>
 
     </div>
   )
